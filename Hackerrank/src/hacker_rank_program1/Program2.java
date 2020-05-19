138e1134578585f5d3e98210f78a005795ef07f1
DEVABRATA SAHOO <devabratasahoo07@outlook.com>
2020-05-19 10:39:24 +0530

    Hacker-rank Level-1 Program

---
+++ Hackerrank/src/hacker_rank_program1/Program2.java
@@ -0,0 +1,15 @@
+package hacker_rank_program1;
+
+public class Program2 {
+
+
+	    public static void main(String[] args) {
+	        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+	        String s1="Hello, World.";
+	        String s2="Hello, Java.";
+	        System.out.println(s1 );
+	         System.out.println(s2 );
+	    }
+	}
+
+
